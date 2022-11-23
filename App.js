c859bf10e0004fcc94f9e1c9af3893d37dec7ca2
Hina Chauhan <hina@digitalflake.com>
2022-11-23 16:00:24 +0530

    demo initial commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.appContainer}>
+      <View style = {styles.inputContainer}>
+        <TextInput  style={styles.textInput} placeholder='Enter a message'/>
+        <Button title='Save'/>
+      </View>
+      <View style = {{paddingTop : 20}}>
+        <Text>List Of Messages....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer : {
+      padding : 16,
+      paddingTop:100
+    },
+    inputContainer : {
+      flexDirection : "row",
+      justifyContent : "space-between"
+      
+    },
+    textInput : {
+      borderWidth : 1,
+      borderColor : "#cccccc",
+      width : "80%"
+    }
 });
