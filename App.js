a861c7e235239ae761133ac7418ae7d6fcbcfd78
Hina Chauhan <hina@digitalflake.com>
2022-11-24 13:19:31 +0530

    custom component for input and handling input

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
   StyleSheet,
@@ -6,20 +5,15 @@ import {
   View,
   Button,
   TextInput,
-  ScrollView,
   FlatList,
 } from "react-native";
 import MessageItem from "./components/MessageItem";
+import MessageInput from "./components/MessageInput";
 
 export default function App() {
-  const [enteredMessage, setMessage] = useState("");
   const [messageArr, setMessageArr] = useState([]);
 
-  function messageInputHandler(enteredText) {
-    setMessage(enteredText);
-  }
-
-  function saveMessageHandler() {
+  function saveMessageHandler(enteredMessage) {
     // console.log(enteredMessage);
     setMessageArr((currentArr) => [
       ...currentArr,
@@ -29,26 +23,16 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter a message"
-          onChangeText={messageInputHandler}
-        />
-        <Button title="Save" onPress={saveMessageHandler} />
-      </View>
+      <MessageInput onMessageAdd={saveMessageHandler} />
       <View style={styles.messageContainer}>
         <Text>List Of Messages....</Text>
         <FlatList
           data={messageArr}
           renderItem={(itemData) => {
-
-            return (
-             <MessageItem text={itemData.item.text}/>
-            );
+            return <MessageItem text={itemData.item.text} />;
           }}
-          keyExtractor={(item,index) => {
-            return item.id
+          keyExtractor={(item, index) => {
+            return item.id;
           }}
           alwaysBounceVertical={false}
         />
@@ -63,23 +47,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingTop: 50,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: "gray",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    padding: 8,
-  },
+
   messageContainer: {
     flex: 9,
   },
-  
 });
---
+++ components/MessageInput.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+
+function MessageInput(props) {
+  const [enteredMessage, setMessage] = useState("");
+  function messageInputHandler(enteredText) {
+    setMessage(enteredText);
+  }
+  function saveMessageHandler() {
+    props.onMessageAdd(enteredMessage);
+    setMessage("");
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter a message"
+        onChangeText={messageInputHandler}
+        value = {enteredMessage}
+      />
+      <Button title="Save" onPress={saveMessageHandler} />
+    </View>
+  );
+}
+
+export default MessageInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    padding: 8,
+  },
+});
